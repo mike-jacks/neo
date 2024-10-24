ae49ec52203f590251c22f7740eb53c8eea9b459
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-10-24 09:45:10 -0600

    fix typo

--- db/neo4jDatabase.go
+++ db/neo4jDatabase.go
@@ -1513,7 +1513,7 @@ func (db *Neo4jDatabase) RenameDomainSchemaNode(ctx context.Context, domain stri
 
 	query := `
 		MATCH (node {_domain: $domain})
-		SET node._name = $newName,
+		SET node._domain = $newName,
 		node._name = CASE WHEN node:DOMAIN_SCHEMA THEN $newName ELSE node._name END
 		WITH node
 		WHERE NOT node:DOMAIN_SCHEMA AND NOT node:TYPE_SCHEMA
