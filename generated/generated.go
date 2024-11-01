014e2827a46d7c2a8e846d52e1b9e174f7c831a6
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-11-01 09:56:26 -0600

    remove id from properties

--- generated/generated.go
+++ generated/generated.go
@@ -146,7 +146,6 @@ type ComplexityRoot struct {
 	}
 
 	Property struct {
-		ID    func(childComplexity int) int
 		Key   func(childComplexity int) int
 		Type  func(childComplexity int) int
 		Value func(childComplexity int) int
@@ -865,13 +864,6 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 
 		return e.complexity.ObjectRelationshipObjectNodesResponse.Success(childComplexity), true
 
-	case "Property.id":
-		if e.complexity.Property.ID == nil {
-			break
-		}
-
-		return e.complexity.Property.ID(childComplexity), true
-
 	case "Property.key":
 		if e.complexity.Property.Key == nil {
 			break
@@ -1423,7 +1415,6 @@ enum PropertyType {
 }
 
 type Property {
-  id: String!
   key: String!
   value: Any!
   type: PropertyType!
@@ -6161,8 +6152,6 @@ func (ec *executionContext) fieldContext_ObjectNode_properties(_ context.Context
 		IsResolver: false,
 		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
 			switch field.Name {
-			case "id":
-				return ec.fieldContext_Property_id(ctx, field)
 			case "key":
 				return ec.fieldContext_Property_key(ctx, field)
 			case "value":
@@ -6707,8 +6696,6 @@ func (ec *executionContext) fieldContext_ObjectRelationship_properties(_ context
 		IsResolver: false,
 		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
 			switch field.Name {
-			case "id":
-				return ec.fieldContext_Property_id(ctx, field)
 			case "key":
 				return ec.fieldContext_Property_key(ctx, field)
 			case "value":
@@ -7214,50 +7201,6 @@ func (ec *executionContext) fieldContext_ObjectRelationshipObjectNodesResponse_o
 				return ec.fieldContext_ObjectRelationshipObjectNode_toObjectNode(ctx, field)
 			}
 			return nil, fmt.Errorf("no field named %q was found under type ObjectRelationshipObjectNode", field.Name)
-		},
-	}
-	return fc, nil
-}
-
-func (ec *executionContext) _Property_id(ctx context.Context, field graphql.CollectedField, obj *model.Property) (ret graphql.Marshaler) {
-	fc, err := ec.fieldContext_Property_id(ctx, field)
-	if err != nil {
-		return graphql.Null
-	}
-	ctx = graphql.WithFieldContext(ctx, fc)
-	defer func() {
-		if r := recover(); r != nil {
-			ec.Error(ctx, ec.Recover(ctx, r))
-			ret = graphql.Null
-		}
-	}()
-	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
-		ctx = rctx // use context from middleware stack in children
-		return obj.ID, nil
-	})
-	if err != nil {
-		ec.Error(ctx, err)
-		return graphql.Null
-	}
-	if resTmp == nil {
-		if !graphql.HasFieldError(ctx, fc) {
-			ec.Errorf(ctx, "must not be null")
-		}
-		return graphql.Null
-	}
-	res := resTmp.(string)
-	fc.Result = res
-	return ec.marshalNString2string(ctx, field.Selections, res)
-}
-
-func (ec *executionContext) fieldContext_Property_id(_ context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
-	fc = &graphql.FieldContext{
-		Object:     "Property",
-		Field:      field,
-		IsMethod:   false,
-		IsResolver: false,
-		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
-			return nil, errors.New("field of type String does not have child fields")
 		},
 	}
 	return fc, nil
@@ -12079,11 +12022,6 @@ func (ec *executionContext) _Property(ctx context.Context, sel ast.SelectionSet,
 		switch field.Name {
 		case "__typename":
 			out.Values[i] = graphql.MarshalString("Property")
-		case "id":
-			out.Values[i] = ec._Property_id(ctx, field, obj)
-			if out.Values[i] == graphql.Null {
-				out.Invalids++
-			}
 		case "key":
 			out.Values[i] = ec._Property_key(ctx, field, obj)
 			if out.Values[i] == graphql.Null {
--- model/models_gen.go
+++ model/models_gen.go
@@ -101,7 +101,6 @@ type ObjectRelationshipObjectNodesResponse struct {
 }
 
 type Property struct {
-	ID    string       `json:"id"`
 	Key   string       `json:"key"`
 	Value any          `json:"value"`
 	Type  PropertyType `json:"type"`
